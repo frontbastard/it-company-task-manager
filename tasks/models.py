5f546080ce4d0b12bcf35c0cd55e0afbb8d5f16a
pavlo <frontbastard@gmail.com>
2024-06-13 15:35:00 +0200

    Create models and update settings for task management
        ## New Models
        - `Position` model for worker positions
        - `Worker` model extending `AbstractUser` for worker accounts
        - `TaskType` model for categorizing task types
        - `Task` model for managing tasks and assigning workers
    
        ## Settings Update
        - Set `AUTH_USER_MODEL` to `tasks.Worker` in `settings.py`
        - Add the `tasks` app to INSTALLED_APPS in `settings.py`

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,47 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import TextChoices
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    class PriorityChoices(TextChoices):
+        URGENT = "urgent", "Urgent"
+        HIGH = "high", "High"
+        NORMAL = "normal", "Normal"
+        LOW = "low", "Low"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=PriorityChoices,
+        default=PriorityChoices.NORMAL,
+    ),
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.SET_NULL,
+        null=True,
+    ),
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="tasks"
+    )
