77f7d19ab003a591709a33c70a4d804f9377c342
pavlo <frontbastard@gmail.com>
2024-06-14 00:04:04 +0200

    Customize admin interface for Worker and Task models
    ## Worker Admin
    
    - Registered `WorkerAdmin` class for the custom `Worker` model with the UserAdmin class
    - Added `position` field to the list display
    - Added `position` field to the fieldsets for existing users
    - Added `first_name`, `last_name`, and `position` fields to the add fieldsets for new users
    
    ## Task Admin
    
    - Registered `TaskAdmin` class for the `Task` model
    - Set list display to show `name`, `deadline`, `is_completed`, `priority`, and `task_type` fields
    - Added search functionality for the `name` field
    - Added list filter for the `is_completed` field
    
    ## Position Model
    
    - Registered the `Position` model with the default `ModelAdmin` class

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import Task, Position
+
+
+@admin.register(get_user_model())
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "is_completed", "priority", "task_type")
+    search_fields = ("name",)
+    list_filter = ("is_completed",)
+
+
+admin.site.register(Position)
