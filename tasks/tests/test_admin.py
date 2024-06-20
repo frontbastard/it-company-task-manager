b1c28998e6a53ff1e5c858526fa6b77be79c86fb
pavlo <frontbastard@gmail.com>
2024-06-21 00:42:36 +0200

    Create tests for current functionality
    - Installed module `factory_boy`.
    - Created factories  for all entities.
    - Created tests for admin page to check whether a custom field `position` displayed on the list and detail pages.
    - Created tests for models where the __str__ method is checked for every entity.
    - Created tests for views task, worker and position:
      - Test whether not logged-in users can access a view.
      - Test that entity list can be got successfully for logged-in users.
      - Test search functionality.
      - Test sort functionality.

---
+++ tasks/tests/test_admin.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from tasks.tests.factories import WorkerFactory, PositionFactory
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self) -> None:
+        self.admin_user = get_user_model().objects.create_superuser(
+            username="admin",
+            password="<PASSWORD>"
+        )
+        self.client.force_login(self.admin_user)
+        self.worker = WorkerFactory(position=PositionFactory())
+
+    def test_position_listed(self):
+        url = reverse("admin:tasks_worker_changelist")
+        res = self.client.get(url)
+        self.assertContains(res, self.worker.position)
+
+    def test_worker_detail_position_listed(self):
+        url = reverse("admin:tasks_worker_change", args=[self.worker.id])
+        res = self.client.get(url)
+        self.assertContains(res, self.worker.position)
