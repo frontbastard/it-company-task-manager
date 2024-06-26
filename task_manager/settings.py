dd09c21c175e816c7f4bba309f5f781c9cd150ce
pavlo <frontbastard@gmail.com>
2024-06-26 22:51:07 +0200

    Add more proper settings for ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -56,8 +56,11 @@ LOGGING = {
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "it-company-task-manager-hvco.onrender.com",
 ]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
